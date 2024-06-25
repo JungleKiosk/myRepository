0580309bf74c435c890c00ef536f04cc1782063f
Fra <fra24eso@gmail.com>
2024-06-25 13:58:11 +0200

    app footer & img logo

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -1,11 +1,56 @@
 <script setup>
 
+
 </script>
 
+
 <template>
-  <h1>Footer</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis illum soluta nobis delectus fugiat. Eos in ex
-    quod quae odio ullam veritatis tempora iure repellat sed nobis, deserunt aspernatur perspiciatis!</p>
+  <footer id="footer" class="p-5 bg-dark">
+    <div class="row justify-content-center mt-5">
+
+      <div class="col-sm-4 col-md-3 text-center">
+        <h3 class="">Follow me</h3>
+        <a target="_blank" href="https://github.com/JungleKiosk"><img class="icon_social mx-2"
+            src="../assets/img/logo/github_footer.svg" alt=""></a>
+        <a target="_blank" href="https://www.linkedin.com/in/francescominutella/"><img class="icon_social mx-2"
+            src="../assets/img/logo/linkedin_footer.svg" alt=""></a>
+        <a target="_blank" href="https://www.youtube.com/@kioskgis"><img class="icon_social mx-2"
+            src="../assets/img/logo/youtube_footer.svg" alt=""></a>
+      </div>
+
+    </div>
+
+    <div class="row justify-content-center mt-5">
+
+
+      <div class="col-12 col-lg-2 text-center">
+        <h5>Contacts</h5>
+
+        <span>+39 3311874661</span> <br>
+        <span>eso.fra24@gmail.com</span>
+
+      </div>
+
+    </div>
+  </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.icon_social {
+  width: 10%;
+}
+
+ul li {
+  list-style: none;
+}
+
+#footer{
+  height: 500px;
+}
+
+@media (max-width: 786px) {
+  .icon_social {
+    width: 20%;
+  }
+}
+</style>
