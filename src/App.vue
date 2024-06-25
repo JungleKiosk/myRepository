07920a0200b35c61b76fa6dc8c9f9f8c67cd3c59
fra <eso.fra24@gmail.com>
2024-06-25 12:59:43 +0200

    setting VueRouter: routs & views

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
 </script>
+
 
 <template>
 
-  <HelloWorld />
+
+
+  <div class="container-fluid bg-dark py-4">
+
+    <div class="row justify-content-center ">
+      <div class="col-12 col-lg-3 text-center text-center">
+        <!-- Utilizza RouterLink per creare bottoni -->
+        <RouterLink active-class="active" to="/">
+          <button class="rounded-2 btn_bg_green mx-1">
+            <span>Home</span>
+          </button>
+        </RouterLink>
+
+<!--         <RouterLink active-class="active" to="/getstart">
+          <button class="rounded-2 btn_bg_green mx-1">
+            <span>Get Start</span>
+          </button>
+        </RouterLink> -->
+
+        <RouterLink active-class="active" to="/about">
+          <button class="rounded-2 btn_bg_green mx-1">
+            <span>About me</span>
+          </button>
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+  <RouterView />
 </template>
 
-<style scoped>
+<style>
 
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<script>
+import Footer from '../components/FooterVue.vue'
+
+export default {
+    name: "Home",
+    data() {
+        return {
+        }
+    },
+    components: {
+        Footer
+    }
+}
+
+
+</script>
+
+
+<template>
+    <main>
+        <section class="jumbotron">
+            <div class="container">
+                <div class="row justify-content-center mb-5">
+                    <div class="col-12 text-center mt-5">
+                        <img src="../assets/img/fw_logo.svg" class="text-center" alt="">
+                    </div>
+                    <div class="col-12 col-lg-8 bg_row_img py-5 my-2 align-content-center introduce">
+
+                        <h1>Home</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis minima aliquid tempora,
+                            dicta a officia, maiores nam beatae dolore, quos officiis libero. Tempora eligendi deleniti
+                            similique quibusdam excepturi minima natus.</p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <Footer></Footer>
+</template>
+
+<style>
+.img_card {
+    width: 50%;
+}
+
+.eurostat_logo {
+    width: 50%;
+}
+
+.icon_social {
+    width: 10%;
+}
+
+
+@media (max-width: 786px) {
+    .icon_social {
+        width: 20%;
+    }
+}
+</style>
