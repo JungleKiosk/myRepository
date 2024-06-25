55ca7b4a3398f93c7168321f9f95cd4291066961
Fra <fra24eso@gmail.com>
2024-06-25 13:45:14 +0200

    git config

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -4,8 +4,8 @@
 
 <template>
   <h1>Footer</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis illum soluta nobis delectus fugiat. Eos in ex
+    quod quae odio ullam veritatis tempora iure repellat sed nobis, deserunt aspernatur perspiciatis!</p>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
